c04d9ac3e19819a45f9d77f6fd914bfdbf3d3f4d
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-10-14 16:22:30 +0530

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,18 +3,18 @@
 go() {
     mkdir $1 || return
     echo 'Done'
-    cp ~/.template/sol.cpp $1/sol.cpp
-    # cp ~/.template/gen.cpp $1/gen.cpp
-    # cp ~/.template/s.sh $1/s.sh
+    cp ~/.template/sol.cpp $1/sol.cpp    # solution file
+    cp ~/.template/gen.cpp $1/gen.cpp    # testcases generator file
+    cp ~/.template/s.sh $1/s.sh          # stress testing bash script
+    
+    # Without random quote
     # echo -e "/*\nAuthor: Nachiket Kanore\nCreated: $(date)\n*/\n$(cat $1/sol.cpp)" > $1/sol.cpp
 
-    # name=$(shuf -n 1 ~/.template/quotes)
-    # echo $name
-
+    # With random quote
     echo -e "*/\n$(cat $1/sol.cpp)" > $1/sol.cpp
     echo -e "(ツ) $(shuf -n 1 ~/.template/quotes)\n$(cat $1/sol.cpp)" > $1/sol.cpp
     echo -e "Created: $(date)\n$(cat $1/sol.cpp)" > $1/sol.cpp
     echo -e "Author: Nachiket Kanore\n$(cat $1/sol.cpp)" > $1/sol.cpp
     echo -e "/*\n$(cat $1/sol.cpp)" > $1/sol.cpp
-    subl $1/sol.cpp
+    subl $1/sol.cpp                     # you can change this to open in your desired editor
 }
